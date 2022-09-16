e3730259ea95d591105038f17595c8f77b18b700
Akinwale Damilare <damilareakinwale0@gmail.com>
2022-09-16 10:35:33 -0700

    print 0 to 9 with 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char i = 0;
+
+	while ( i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + n);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
