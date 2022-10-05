c00d9a30928d59221cdf16eee6a921f518aa4701
Akinwale Damilare <damilareakinwale0@gmail.com>
2022-10-05 06:44:49 -0700

    static lib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ *
+ *  * _puts - Entry function
+ *
+ *   * @str: pointer for char
+ *
+ *    * Return: 0
+ *
+ *     */
+
+void _puts(char *str)
+
+{
+
+	puts(str);
+
+}
