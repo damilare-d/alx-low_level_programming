b59360135801bb609bf570a5dffe6ef132e829a9
Akinwale Damilare <damilareakinwale0@gmail.com>
2022-10-11 15:40:11 -0700

    preprocessor tasks

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef ABS_FUNCTION
+
+#define ABS_FUNCTION
+
+#define ABS(X) ((X) < 0 ? -1 * (X) : (X))
+
+
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef FILE_SUM
+
+#define FILE_SUM
+
+#define SUM(X, Y) ((X) + (Y))
+
+
+
+
+
+#endif
