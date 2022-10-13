9da1b68d5a1d13f54d1cd04f886772c400dda559
Akinwale Damilare <damilareakinwale0@gmail.com>
2022-10-12 17:37:09 -0700

    more fixes

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,21 +4,17 @@
 
 /**
  *
- *  * int_index - searches for an integer
+ * int_index - searches for an integer
  *
- *   * @array: array of elemnts
+ * @array: array of elemnts
  *
- *    * @size: number of elements in the array
+ * @size: number of elements in the array
  *
- *     * @cmp: is a pointer to the function to be used to compare values
- *
- *      * Return: index first element cmp function does not return 0
- *
- *       * If no element matches, return -1
- *
- *        * If size <= 0, return -1
- *
- *         */
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return: index first element cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+ */
 
 
 
@@ -28,58 +24,37 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 		int i = 0;
 
-			int flag;
+		int flag;
+	    	if (array && size && cmp)
+	    	{
+		if (size <= 0)
+		{
+		    	flag = -1;
+								    		}
+										else
+	{
+										    while (i < size)
+								    		    {
+																    			if ((*cmp)(array[i]) != 0)
 
+																			{
+																			  flag = i;
+							    			    break;
 
+	}
+										else
 
-			    	if (array && size && cmp)
+	{
+									    flag = -1;
 
-					    	{
+						    			}
 
-									if (size <= 0)
+i++;
+								    }
 
-												{
+											    		}
+										  }
 
-															    flag = -1;
-
-															    		}
-
-											else
-
-														{
-
-																	    while (i < size)
-
-																		    		    {
-
-																					    			if ((*cmp)(array[i]) != 0)
-
-																												{
-
-																																    flag = i;
-
-																																    			    break;
-
-																																			    			}
-
-																											else
-
-																															{
-
-																																			    flag = -1;
-
-																																			    			}
-
-																														i++;
-
-																																    }
-
-																	    		}
-
-												  }
-
-
-
-				    return (flag);
+	    return (flag);
 
 }
